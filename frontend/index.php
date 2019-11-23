039aa33496690a28309bfe17f6085ba2d7189e4b
roulzin <janos.peter.tasnv@gmail.com>
2019-11-23 23:25:34 +0100

    Oprava pohubenej stranky calej

--- frontend/index.php
+++ frontend/index.php
@@ -2,22 +2,24 @@
 include "layout/header.php";
 include "layout/nav.php";
 ?>
-<article>
-    <section class="index-article">
+<article class="index-article">
+    <section >
         <div class="heading-container">
             <h1 class="heading"><?php echo $store_name;?></h1>
             <p>Lorem ipsum hello my name is peter dzejnous a vítam ťa na svojej paradnej stránke</p>
         </div>
         <div class="menu">
-            <a href="#"><span class="dot-active"></span></a>
             <a href="#"><span class="dot"></span></a>
             <a href="#"><span class="dot"></span></a>
-        </div>
-        <div>
-
+            <a href="#"><span class="dot"></span></a>
         </div>
     </section>
-
+    <div class="clear"></div>
 </article>
+
+<section class="about-us">
+    <h1>About Us</h1>
+
+</section>
 <?php
 include "layout/footer.php";
--- frontend/layout/nav.php
+++ frontend/layout/nav.php
@@ -9,7 +9,7 @@ $currentPage = $_SERVER['REQUEST_URI'];
             <li <?php if ($currentPage == '/pages/products.php'){echo 'class="nav-item-active"';}else{echo'class="nav-item"';} ?>><a href="../pages/products.php">Products</a></li>
             <li <?php if ($currentPage == '/pages/blog.php'){echo 'class="nav-item-active"';}else{echo'class="nav-item"';} ?>><a href="../pages/blog.php">Blog</a></li>
             <li <?php if ($currentPage == '/pages/contact.php'){echo 'class="nav-item-active"';}else{echo'class="nav-item"';} ?>><a href="../pages/contact.php">Contact</a></li>
-            <li class="nav-item-right"><a href="../pages/login.php">Sign In</a></li>
+            <li class="nav-item-right"><a href="../pages/login.php">Log In</a></li>
     </ul>
 
 </nav>
