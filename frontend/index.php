e6f469d66e74cdff4e53b8fb795f4c0d60eea3ad
roulzin <janos.peter.tasnv@gmail.com>
2019-11-23 18:00:59 +0100

    Kus som sa pobavil s indexom... ale len kus

--- frontend/index.php
+++ frontend/index.php
@@ -4,10 +4,20 @@ include "layout/nav.php";
 ?>
 <article>
     <section class="index-article">
-        <h1 class="heading"><?php echo $store_name;?></h1>
+        <div class="heading-container">
+            <h1 class="heading"><?php echo $store_name;?></h1>
+            <p>Lorem ipsum hello my name is peter dzejnous a vítam ťa na svojej paradnej stránke</p>
+        </div>
+        <div class="menu">
+            <a href="#"><span class="dot-active"></span></a>
+            <a href="#"><span class="dot"></span></a>
+            <a href="#"><span class="dot"></span></a>
+        </div>
+        <div>
 
+        </div>
     </section>
-    <p>asdad</p>
+
 </article>
 <?php
 include "layout/footer.php";
