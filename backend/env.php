2a2da43e9c80e766f6d44e8a69b65fe99f009594
roulzin <janos.peter.tasnv@gmail.com>
2019-11-25 19:54:18 +0100

    Zaš niiič

---
+++ backend/env.php
@@ -0,0 +1,8 @@
+<?php
+$title = "Brano shop";
+$time_zone = "Europe/Bratislava";
+
+$DB_server = "localhost";
+$DB_name = "eshop";
+$DB_username = "root";
+$DB_password = "";
--- frontend/index.php
+++ frontend/index.php
@@ -10,13 +10,8 @@ include "layout/nav.php";
         <div class="heading-container">
             <h1 class="heading">
                 <?php echo $store_name;?>
-<<<<<<< HEAD
             </h1>
             <p>Lorem ipsum hello my name is peter dzejnous a vítam ťa na svojej paradnej stránke</p>
-=======
-                <p>Lorem ipsum hello my name is peter dzejnous a vítam ťa na svojej paradnej stránke</p>
-            </h1>
->>>>>>> 228c18b1260077de7d823b29355160417b937f96
         </div>
         <div class="menu">
             <a href="#"><span class="dot"></span></a>
@@ -28,11 +23,7 @@ include "layout/nav.php";
 </article>
 
 <section class="about-us">
-<<<<<<< HEAD
     <h1>Newest arrivals</h1>
-=======
-    <h1>About Us</h1>
->>>>>>> 228c18b1260077de7d823b29355160417b937f96
 
 </section>
 <?php
