a4d9e7913a427e40f00bc7dbaa5a9a8a0032c0aa
Glatoo <darkisharki@gmail.com>
2019-11-26 16:08:52 +0100

    Pridane produkty a clanky

--- frontend/pages/blog.php
+++ frontend/pages/blog.php
@@ -9,7 +9,16 @@ include "../controllers/blogController.php"
         <p style="color: white">
             <?php
             $articles = get_articles(20);
-            echo json_encode($articles);
+            echo "<h1 style='color: #fff;'>Pocet produktov: " . count($articles) . "ks</h1>";
+            foreach ($articles as $article) {
+                echo "<div class='article-container' style='color: white'>";
+                echo "<h1 class='article-header'>" . $article[0] .". ". $article[1]. "</h1>";
+                echo "<p class='article-content'>" . $article[2] . "</p>";
+                echo "<i class='article-author'>" . $article[3] . "</i><br>";
+                echo "<p class='article-date'>" . $article[4] . "</p>";
+                echo "</div>";
+            }
+
             ?>
         </p>
     </section>
--- frontend/pages/products.php
+++ frontend/pages/products.php
@@ -9,11 +9,13 @@ include "../controllers/productsController.php"
         <p style="color: white">
         <?php
             $products = get_products(20);
+            echo "<h1 style='color: #fff;'>Pocet produktov: ".count($products)."ks</h1>";
             foreach ($products as $product) {
-                echo "<div class='product-container'>";
-                echo "<h3>".$product["title"]."</h3>";
-                echo "<p>".$product["description"]."</p>";
-                echo "<i>".$product["price"]."</i>";
+                echo "<div class='product-container' style='color: white'>";
+                echo "<h1 class='product-header'>" . $product[0] .". ". $product[1]. "</h1>";
+                echo "<p class='product-description'>" . $product[2] . "</p>";
+                echo "<i class='product-price'>" . $product[3] . "</i><br>";
+                echo "<p class='product-img'>" . $product[4] . "</p>";
                 echo "</div>";
             }
         ?>
